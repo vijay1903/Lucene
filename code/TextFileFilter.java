7c0111d723e486c7a41a23ab0b73deb8ed82ff2e
Vijay Vishwakarma <vijayv@mail.usf.edu>
2018-04-26 21:59:30 -0400

    Add files via upload

---
+++ code/TextFileFilter.java
@@ -0,0 +1,9 @@
+package com.firstapp.lucene;
+import java.io.File;
+import java.io.FileFilter;
+
+public class TextFileFilter implements FileFilter {
+	@Override
+	public boolean accept(File pathname) {
+	return pathname.getName().toLowerCase().endsWith(".txt"); }
+}
